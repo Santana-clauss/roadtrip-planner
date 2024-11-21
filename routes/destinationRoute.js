67ef42844b3398764a25ecd07b936d81f601de7f
santana clauss <jepchumbasantana@gmail.com>
2024-11-22 02:58:27 +0300

    updated routes

--- routes/destinationRoute.js
+++ routes/destinationRoute.js
@@ -11,9 +11,9 @@ const {
 const router = express.Router();
 
 //routes
-router.post('/destinations', addDestination); // Add a destination
-router.get('/destinations', getDestinations); // Get all destinations
-router.put('/destinations/reorder', reorderDestinations); // Rearrange destinations
+router.post('/destinations', addDestination); // Add a destination --tested
+router.get('/destinations', getDestinations); // Get all destinations --tested
+router.put('/destinations/reorder', reorderDestinations); // Rearrange destinations --tested
 router.get('/route', getRoute); //  route details
 router.get('/summary', getSummary); // journey summary
 
